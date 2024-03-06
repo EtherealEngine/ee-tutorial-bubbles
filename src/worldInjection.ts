e25085314577db8604885657932859c994b34457
HexaField <joshfield999@gmail.com>
2024-03-06 14:11:29 +1100

    Add back optional worldInjection async function

--- src/worldInjection.ts
+++ src/worldInjection.ts
@@ -1,7 +1,9 @@
 import { isClient } from '@etherealengine/common/src/utils/getEnvironment'
 
-/** Ensure bubble system is imported */
+/** Ensure bubble system is imported on both client and server */
 import './systems/BubbleSystem'
 
 /** Ensure bubble editor is imported only on the client */
-if (isClient) import('./editors/RegisterBubbleEditor')
+export default async function () {
+  if (isClient) await import('./editors/RegisterBubbleEditor')
+}
